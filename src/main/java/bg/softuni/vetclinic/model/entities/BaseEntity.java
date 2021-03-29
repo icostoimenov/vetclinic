1ae72d697ab971f04b79da211ff3ffa036cfe9e2
icostoimenov <58812603+icostoimenov@users.noreply.github.com>
2021-03-29 16:29:14 +0300

    Initial HTML & CSS Skeleton

---
+++ src/main/java/bg/softuni/vetclinic/model/entities/BaseEntity.java
@@ -0,0 +1,22 @@
+package bg.softuni.vetclinic.model.entities;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false, updatable = false)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public BaseEntity setId(long id) {
+        this.id = id;
+        return this;
+    }
+}
+
